344433659654d3863ab95c786fe9253a594cd9e6
Giovanni Lovato <giovanni@lova.to>
2022-09-01 15:02:38 +0200

    chore(demo): Demo improvements (#31)

--- auth-demo/src/main/java/com/vaadin/auth/demo/AuthDemoApplication.java
+++ auth-demo/src/main/java/com/vaadin/auth/demo/AuthDemoApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
  */
+@Theme("auth-demo")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 @SpringBootApplication
 public class AuthDemoApplication extends SpringBootServletInitializer
         implements AppShellConfigurator {
--- auth-demo/src/main/java/com/vaadin/auth/demo/view/MainLayout.java
+++ auth-demo/src/main/java/com/vaadin/auth/demo/view/MainLayout.java
@@ -1,95 +1,69 @@
 package com.vaadin.auth.demo.view;
 
+import com.vaadin.auth.demo.components.nav.Nav;
+import com.vaadin.auth.demo.components.nav.NavItem;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.RouterLink;
 
 /**
  * The main layout used by other application views.
  */
 public class MainLayout extends AppLayout {
 
-    private final H1 appTitle = new H1();
-
     private final H2 viewTitle = new H2();
 
-    private final HorizontalLayout wrapper;
-
-    private final VerticalLayout viewHeader;
-
     public MainLayout() {
-        appTitle.setText("Auth Demo");
-
-        // @formatter:off
-        appTitle.getStyle()
-                .set("font-size", "var(--lumo-font-size-l)")
-                .set("line-height", "var(--lumo-size-l)")
-                .set("margin", "0 var(--lumo-space-m)");
-        // @formatter:on
-
-        Tabs views = getPrimaryNavigation();
-
-        DrawerToggle toggle = new DrawerToggle();
-
-        // @formatter:off
-        viewTitle.getStyle()
-                .set("font-size", "var(--lumo-font-size-l)")
-                .set("margin", "0");
-        // @formatter:on
-
-        this.wrapper = new HorizontalLayout(toggle, viewTitle);
-        wrapper.setAlignItems(Alignment.CENTER);
-        wrapper.setSpacing(false);
-        wrapper.setWidthFull();
-
-        this.viewHeader = new VerticalLayout(wrapper);
-        viewHeader.setPadding(false);
-        viewHeader.setSpacing(false);
-
-        addToDrawer(appTitle, views);
-        addToNavbar(viewHeader);
-
         setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
     }
 
-    private Tabs getPrimaryNavigation() {
-        Tabs tabs = new Tabs();
-        tabs.add(createTab(new Icon("vaadin", "home"), "Home", HomeView.class));
-        tabs.add(createTab(new Icon("vaadin", "user"), "Profile",
-                ProfileView.class));
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        tabs.setSelectedIndex(1);
-        return tabs;
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassNames("view-toggle");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle.addClassNames("view-title");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("view-header");
+        return header;
     }
 
-    private Tab createTab(Icon icon, String viewName,
-            Class<? extends Component> viewClass) {
-        // @formatter:off
-        icon.getStyle()
-                .set("box-sizing", "border-box")
-                .set("margin-inline-end", "var(--lumo-space-m)")
-                .set("padding", "var(--lumo-space-xs)");
-        // @formatter:on
+    private Component createDrawerContent() {
+        H1 appName = new H1("Auth Demo");
+        appName.addClassNames("app-name");
 
-        RouterLink link = new RouterLink();
-        link.add(icon, new Span(viewName));
+        Div section = new Div(appName, createNavigation(), createFooter());
+        section.addClassNames("drawer-section");
+        return section;
+    }
 
-        link.setRoute(viewClass);
-        link.setTabIndex(-1);
+    private Nav createNavigation() {
+        Nav nav = new Nav();
+        nav.addClassNames("app-nav");
 
-        return new Tab(link);
+        nav.addItem(new NavItem("Home", HomeView.class, "la la-home"));
+        nav.addItem(new NavItem("Profile", ProfileView.class, "la la-user"));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("app-nav-footer");
+
+        return layout;
     }
 
     private void setViewTitle(String title) {
