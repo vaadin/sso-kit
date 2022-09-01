6f1b85a72515339df71f60a744af29689c5ef758
Diego Cardoso <diego@vaadin.com>
2022-09-01 14:29:27 +0300

    docs: add documentation for VaadinAuthContext interface

--- auth-starter/src/main/java/com/vaadin/auth/starter/VaadinAuthContext.java
+++ auth-starter/src/main/java/com/vaadin/auth/starter/VaadinAuthContext.java
@@ -4,6 +4,18 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 import java.util.Optional;
 
+/**
+ * An interface to access the authentication context of the application
+ *
+ * @author Vaadin Ltd
+ * @since 1.0
+ */
 public interface VaadinAuthContext {
+    /**
+     * Gets the authenticated user on the application done by the SSO providers
+     *
+     * @return an optional {@link OidcUser} object if the user is authenticated
+     *         or an optional {@code null} otherwise
+     */
     Optional<OidcUser> getAuthenticatedUser();
 }
