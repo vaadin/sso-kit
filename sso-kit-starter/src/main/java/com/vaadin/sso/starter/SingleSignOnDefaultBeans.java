769dc9798a6b4a12fb3ad02f56e39f1cb95bdb82
Giovanni Lovato <giovanni@lova.to>
2022-09-27 14:35:33 +0200

    Configure only with registered clients

--- sso-kit-starter/src/main/java/com/vaadin/sso/starter/SingleSignOnDefaultBeans.java
+++ sso-kit-starter/src/main/java/com/vaadin/sso/starter/SingleSignOnDefaultBeans.java
@@ -1,8 +1,8 @@
 package com.vaadin.sso.starter;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 
@@ -12,7 +12,7 @@ import org.springframework.security.core.session.SessionRegistryImpl;
  * @author Vaadin Ltd
  * @since 1.0
  */
-@Configuration
+@AutoConfiguration
 public class SingleSignOnDefaultBeans {
 
     /**
