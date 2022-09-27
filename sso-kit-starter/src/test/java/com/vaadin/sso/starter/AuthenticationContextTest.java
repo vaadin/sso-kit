9156db9086fbeb394fc0b620698fceb7504c3592
Giovanni Lovato <giovanni@lova.to>
2022-09-27 23:57:31 +0200

    Fix tests

--- sso-kit-starter/src/test/java/com/vaadin/sso/starter/AuthenticationContextTest.java
+++ sso-kit-starter/src/test/java/com/vaadin/sso/starter/AuthenticationContextTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.internal.CurrentInstance;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinResponse;
@@ -26,6 +28,8 @@ import com.vaadin.sso.starter.SingleSignOnConfiguration.DefaultAuthenticationCon
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -65,7 +69,7 @@ public class AuthenticationContextTest {
     }
 
     @Test
-    public void logoutInvokesHandlersOnCurrentRequest()
+    public void logoutInvokesHandlersOnCurrentUI()
             throws IOException, ServletException {
         var req = mock(VaadinServletRequest.class);
         var httpReq = mock(HttpServletRequest.class);
@@ -76,6 +80,10 @@ public class AuthenticationContextTest {
         var httpRes = mock(HttpServletResponse.class);
         when(res.getHttpServletResponse()).thenReturn(httpRes);
         CurrentInstance.set(VaadinResponse.class, res);
+
+        var ui = mock(UI.class, Answers.RETURNS_DEEP_STUBS);
+        doCallRealMethod().when(ui).accessSynchronously(any());
+        CurrentInstance.set(UI.class, ui);
 
         var auth = mock(Authentication.class);
         SecurityContextHolder.setContext(new SecurityContextImpl(auth));
