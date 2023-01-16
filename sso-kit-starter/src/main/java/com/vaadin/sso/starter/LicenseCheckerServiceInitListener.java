c2f1d41b88626cab55b9585a537e097417fe347d
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-01-16 12:44:46 +0200

    chore: Add usage stats (#62)
    Co-authored-by: Giovanni Lovato <giovanni@lova.to>

--- sso-kit-starter/src/main/java/com/vaadin/sso/starter/LicenseCheckerServiceInitListener.java
+++ sso-kit-starter/src/main/java/com/vaadin/sso/starter/LicenseCheckerServiceInitListener.java
@@ -11,6 +11,7 @@ package com.vaadin.sso.starter;
 
 import java.io.IOException;
 
+import com.vaadin.flow.internal.UsageStatistics;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.pro.licensechecker.BuildType;
@@ -36,19 +37,21 @@ public class LicenseCheckerServiceInitListener
     public void serviceInit(ServiceInitEvent event) {
         final var service = event.getSource();
 
-        // Check the license at runtime if in development mode
-        if (!service.getDeploymentConfiguration().isProductionMode()) {
-            try {
-                final var properties = loadAllProperties(PROPERTIES_RESOURCE);
-                final var version = properties.getProperty(VERSION_PROPERTY);
+        try {
+            final var properties = loadAllProperties(PROPERTIES_RESOURCE);
+            final var version = properties.getProperty(VERSION_PROPERTY);
 
+            UsageStatistics.markAsUsed(PRODUCT_NAME, version);
+
+            // Check the license at runtime if in development mode
+            if (!service.getDeploymentConfiguration().isProductionMode()) {
                 // Using a null BuildType to allow trial licensing builds
                 // The variable is defined to avoid method signature ambiguity
                 BuildType buildType = null;
                 LicenseChecker.checkLicense(PRODUCT_NAME, version, buildType);
-            } catch (IOException e) {
-                throw new ExceptionInInitializerError(e);
             }
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
         }
     }
 }
