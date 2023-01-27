10edad637af936a214b9f7bf7b529a7b9d6dacfd
Luciano Vernaschi <luciano@vaadin.com>
2023-01-27 17:26:23 +0100

    Add missing copyrights

--- sso-kit-starter/src/test/java/com/vaadin/sso/starter/SingleSignOnConfigurationTest.java
+++ sso-kit-starter/src/test/java/com/vaadin/sso/starter/SingleSignOnConfigurationTest.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package com.vaadin.sso.starter;
 
 import org.junit.jupiter.api.BeforeEach;
