cb1ecf91a429ddd2491524497e5562f004e85f3a
tamasmak <maktamas@gmail.com>
2023-03-08 08:58:21 +0100

    Add authentication information and basic functionalities to the SsoKit class

--- sso-kit-client/sso-kit-client-lit/src/SsoKit.ts
+++ sso-kit-client/sso-kit-client-lit/src/SsoKit.ts
@@ -1,37 +1,43 @@
-import type { Subscription } from "@hilla/frontend";
+import { logout, Subscription } from "@hilla/frontend";
 import { Buffer } from "buffer";
 import { makeAutoObservable } from "mobx";
 
 declare global {
     const Hilla: {
-        BootstrapSSO?: string;
+        SSO?: string;
     };
 }
 
-type BootstrapData = {
+type SingleSignOnData = {
     authenticated: boolean;
     roles: string[];
-    logoutUrl: string | undefined;
-    registeredProviders: string[];
+    loginLink: string | undefined;
+    logoutLink: string | undefined;
     backChannelLogoutEnabled: boolean;
 }
 
-export type AccessProps = {
+type AccessProps = {
     requiresLogin?: boolean;
-};
+}
 
 /**
  * A store for authentication information
  */
 class SsoKit {
+    /**
+     * If true, the user has been authenticated
+     */
     authenticated = false;
 
+    /**
+     * The user roles
+     */
     roles: string[] = [];
 
     /**
-     * A list of authentication providers. The first provider is the main provider.
+     * The URL which will be called to log in to the SSO provider
      */
-    registeredProviders: string[] = [];
+    loginUrl: string | undefined = undefined;
 
     /**
      * The URL which will be called to log out from the SSO provider
@@ -49,6 +55,11 @@ class SsoKit {
     backChannelLogoutHappened = false;
 
     /**
+     * A list of authentication providers. The first provider is the main provider.
+     */
+    registeredProviders: string[] = [];
+
+    /**
      * The subscription to the back-channel logout event
      */
     private logoutSubscription: Subscription<any> | undefined;
@@ -56,23 +67,84 @@ class SsoKit {
     constructor() {
         makeAutoObservable(this);
 
-        const authInfo = JSON.parse(Buffer.from(Hilla.BootstrapSSO!, 'base64').toString('utf-8')) as BootstrapData;
+        const authInfo = JSON.parse(Buffer.from(Hilla.SSO!, 'base64').toString('utf-8')) as SingleSignOnData;
         this.authenticated = authInfo.authenticated;
         this.roles = authInfo.roles;
-        this.logoutUrl = authInfo.logoutUrl;
-        this.registeredProviders = authInfo.registeredProviders;
+        this.loginUrl = authInfo.loginLink;
+        this.logoutUrl = authInfo.logoutLink;
         this.backChannelLogoutEnabled = authInfo.backChannelLogoutEnabled;
 
         if (this.authenticated && this.backChannelLogoutEnabled) {
             // @ts-ignore: the imported file might not exist, but in that case backChannelLogoutEnabled will be false
             import("Frontend/generated/BackChannelLogoutEndpoint").then((endpoint) => {
                 this.logoutSubscription = endpoint.subscribe();
-
-                this.logoutSubscription!.onNext(async () => {
+                this.logoutSubscription!.onNext(() => {
                     this.backChannelLogoutHappened = true;
+                    this.logoutSubscription!.cancel();
                 });
             });
         }
+    }
+
+    /**
+     * Clears the authentication information
+     */
+    clearAuthInfo = () => {
+        this.authenticated = false;
+        this.roles = [];
+        this.logoutUrl = undefined;
+        this.backChannelLogoutHappened = false;
+        if (this.logoutSubscription) {
+            this.logoutSubscription = undefined;
+        }
+    }
+
+    /**
+     * Returns true if the user is logged in
+     */
+    get loggedIn() {
+        return this.authenticated;
+    }
+
+    /**
+     * Checks if the user has the given role
+     * @param role the role to check
+     * @returns true if the user has the given role
+     */
+    isUserInRole = (role: string) => {
+        return this.roles.includes(role);
+    }
+
+    /**
+     * Logout from the application and the provider. This function must be called
+     * when the user clicks the logout button, for example.
+     */
+    signOut = async () => {
+        await logout(); // Logout on the server
+        this.logoutUrl && (location.href = this.logoutUrl); // Logout on the provider
+    }
+
+    /**
+     * Login to the provider.
+     */
+    signIn = () => {
+        this.loginUrl && (location.href = this.loginUrl); // Redirect to the provider login page
+    }
+
+    /**
+     * Logout from the application and clear the users authentication information.
+     */
+    stayOnPage = async () => {
+        await logout(); // Logout on the server
+        this.clearAuthInfo(); // Logout on the client
+    }
+
+    /**
+     * Logout from the application and redirect the user to the provider login page
+     */
+    loginAgain = async () => {
+        await logout(); // Logout on the server
+        this.loginUrl && (location.href = this.loginUrl); // Redirect to the provider login page
     }
 }
 
