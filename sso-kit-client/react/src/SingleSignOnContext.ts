11049f90873561bc6ef56dddc4df06df0a436986
tamasmak <maktamas@gmail.com>
2023-04-21 00:41:12 +0200

    Update SingleSignOnContext for react

--- sso-kit-client/react/src/SingleSignOnContext.ts
+++ sso-kit-client/react/src/SingleSignOnContext.ts
@@ -22,7 +22,7 @@ type Message = {
 };
 
 /**
- * Definition of the back-channel logout callbacks.
+ * Definition of the function that is executed when back-channel logout happens.
  */
 type LogoutCallback = () => void;
 
@@ -33,39 +33,40 @@ type LogoutCallback = () => void;
  */
 export class SingleSignOnContext {
   /**
-   * The authenticated user.
-   */
-  user?: User;
-
-  /**
-   * If true, the user has been authenticated.
+   * Indicates if the user has been authenticated.
    */
   authenticated = false;
 
   /**
-   * The user roles.
+   * Contains the user's roles.
    */
   roles: string[] = [];
 
   /**
-   * The URL which will be called to log in to the authentication provider.
+   * The URL to call to log in to the authentication provider.
    */
   loginUrl: string;
 
   /**
-   * The URL which will be called to log out from the authentication provider.
+   * The URL to call to log out from the authentication provider.
    */
   logoutUrl?: string;
 
   /**
-   * If true, the application will listen to the back-channel logout events.
+   * A list of the registered authentication provider registration ids.
    */
-  backChannelLogoutEnabled = false;
+  #registrationIds?: Promise<string[]>;
 
   /**
-   * A list of the authentication providers.
+   * Contains information about the authenticated user.
    */
-  registeredProviders: string[] = [];
+  #user?: Promise<User>;
+
+  /**
+   * Indicates if the back-channel logout is enabled.
+   * If true, the application will listen to the back-channel logout events.
+   */
+  #backChannelLogoutEnabled = false;
 
   /**
    * The subscription to the back-channel logout event.
@@ -82,47 +83,28 @@ export class SingleSignOnContext {
     this.roles = singleSignOnData.roles;
     this.loginUrl = singleSignOnData.loginLink;
     this.logoutUrl = singleSignOnData.logoutLink;
-    this.backChannelLogoutEnabled = singleSignOnData.backChannelLogoutEnabled;
+    this.#backChannelLogoutEnabled = singleSignOnData.backChannelLogoutEnabled;
 
-    // @ts-ignore: the imported file might not exist,
-    // in that case the registeredProviders will be empty
-    import("Frontend/generated/SingleSignOnEndpoint.ts")
-      .then(
-        (endpoint) => endpoint.getRegisteredProviders(),
-        (reason) => {
-          throw new EndpointImportError("SingleSignOnEndpoint", reason);
-        }
-      )
-      .then(
-        (registeredProviders: string[]) => {
-          this.registeredProviders = registeredProviders;
-        },
-        (reason: string) => {
-          throw new Error(`Couldn't get registered providers: ${reason}`);
-        }
-      );
+    this.#registrationIds = import(
+      // @ts-ignore
+      "Frontend/generated/SingleSignOnEndpoint.ts"
+      ).then(
+      (endpoint) => endpoint.getRegisteredProviders(),
+      (reason) => {
+        throw new EndpointImportError("SingleSignOnEndpoint", reason);
+      }
+    );
 
-    // @ts-ignore: the imported file might not exist,
-    // in that case the authenticated user will be undefined
-    import("Frontend/generated/UserEndpoint.ts")
-      .then(
-        (endpoint) => endpoint.getAuthenticatedUser(),
-        (reason) => {
-          throw new EndpointImportError("UserEndpoint", reason);
-        }
-      )
-      .then(
-        (user: User) => {
-          this.user = user;
-        },
-        (reason: string) => {
-          throw new Error(`Couldn't get authenticated user: ${reason}`);
-        }
-      );
+    // @ts-ignore
+    this.#user = import("Frontend/generated/UserEndpoint.ts").then(
+      (endpoint) => endpoint.getAuthenticatedUser(),
+      (reason) => {
+        throw new EndpointImportError("UserEndpoint", reason);
+      }
+    );
 
-    if (this.authenticated && this.backChannelLogoutEnabled) {
-      // @ts-ignore: the imported file might not exist,
-      // in that case the backChannelLogoutEnabled will be false
+    if (this.authenticated && this.#backChannelLogoutEnabled) {
+      // @ts-ignore
       import("Frontend/generated/BackChannelLogoutEndpoint.ts")
         .then(
           (endpoint) => endpoint.subscribe(),
@@ -150,17 +132,65 @@ export class SingleSignOnContext {
   }
 
   /**
+   * Updates the single sign-on data in the context.
+   *
+   * @returns a Promise void
+   */
+  fetchSingleSignOnData = () => {
+    // @ts-ignore
+    return import("Frontend/generated/SingleSignOnEndpoint.ts")
+      .then(
+        (endpoint) => endpoint.fetchAll(),
+        (reason) => {
+          throw new EndpointImportError("SingleSignOnEndpoint", reason);
+        }
+      )
+      .then(
+        (singleSignOnData: SingleSignOnData) => {
+          this.authenticated = singleSignOnData.authenticated;
+          this.roles = singleSignOnData.roles;
+          this.loginUrl = singleSignOnData.loginLink;
+          this.logoutUrl = singleSignOnData.logoutLink;
+          this.#backChannelLogoutEnabled =
+            singleSignOnData.backChannelLogoutEnabled;
+        },
+        (reason: string) => {
+          throw new Error(`Couldn't fetch single sign-on data: ${reason}`);
+        }
+      );
+  };
+
+  /**
+   * Gets a list of the registered authentication providers' registration ids.
+   *
+   * @returns a Promise with a list of the registration ids
+   */
+  getRegistrationIds = (): Promise<string[]> | undefined => {
+    return this.#registrationIds;
+  };
+
+  /**
+   * Gets the authenticated user.
+   *
+   * @returns a Promise with the user object
+   */
+  getUser = (): Promise<User> | undefined => {
+    return this.#user;
+  };
+
+  /**
    * Checks if the user has access to the given route.
    *
    * @param route the route to check
    * @returns true if the user has access to the given route, false otherwise
    */
-  hasUserAccess = (route: AccessProps) => {
+  hasAccess = (route: AccessProps) => {
     return !route.requiresLogin || this.authenticated;
   };
 
   /**
    * Checks if the user has the given role.
+   *
    * @param role the role to check
    * @returns true if the user has the given role, false otherwise
    */
@@ -180,29 +210,15 @@ export class SingleSignOnContext {
    */
   logout = async () => {
     await logout();
-    this.#clearSingleSignOnData();
     window.location.href = this.logoutUrl!;
   };
 
   /**
-   * Adds a callback to the back-channel logout subscription callbacks.
+   * Adds a function that is executed when the back-channel logout happens.
    *
    * @param callback a function executed when back-channel logout happens
    */
   onBackChannelLogout(callback: LogoutCallback) {
     this.#logoutSubscriptionCallback = callback;
   }
-
-  /**
-   * Clears the authentication information.
-   */
-  #clearSingleSignOnData = () => {
-    this.authenticated = false;
-    this.roles = [];
-    this.#logoutSubscriptionCallback = undefined;
-    if (this.#logoutSubscription) {
-      this.#logoutSubscription.cancel();
-      this.#logoutSubscription = undefined;
-    }
-  };
 }
