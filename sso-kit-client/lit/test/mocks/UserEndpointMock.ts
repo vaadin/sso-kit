6887ff12598d7e5ec3b4afda52b600c960bf591c
Zhe Sun <zhe@vaadin.com>
2026-09-15 11:07:16 +0300

    Revert "chore: upgrade to Vaadin 25.3, Spring Boot 4.1.1 and react-router 8 (#230)"
    This reverts commit 757e90a6f0bb868e33e59020a5aa9dd41429f378.

--- sso-kit-client/lit/test/mocks/UserEndpointMock.ts
+++ sso-kit-client/lit/test/mocks/UserEndpointMock.ts
@@ -5,13 +5,3 @@
  *
  * See <https://vaadin.com/commercial-license-and-service-terms> for the full license.
  */
-import type { User } from '../../../core/src/User.js';
-
-export const authenticatedUser: User = {
-  fullName: 'John Doe',
-  preferredUsername: 'john'
-};
-
-export function getAuthenticatedUser(): Promise<User> {
-  return Promise.resolve(authenticatedUser);
-}
